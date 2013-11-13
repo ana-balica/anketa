32abda0628457fe60bc41e009653aa735c564d50
ana-balica <ana.balica@gmail.com>
2013-11-13 23:58:36 +0100

    define empty methods for the MultipleChoiseAnswerImpl class

---
+++ src/Poll/PollBundle/Entity/Choice/MultipleChoiceAnswerImpl.php
@@ -0,0 +1,99 @@
+<?php
+namespace Poll\PollBundle\Entity\Choice;
+
+use Poll\PollBundle\Entity\Respondent;
+use Poll\PollBundle\Entity\Question;
+
+/**
+ * Implementation of MultipleChoiseAnswer interface
+ * @author AnaBalica
+ */
+class MultipleChoiceAnswerImpl implements MultipleChoiceAnswer {
+
+	/**
+	 * @return string identifikator objektu
+	 */
+	public function getId() {
+		
+	}
+
+	/**
+	 * 
+	 * @param Option $option
+	 * @return MultipleChoiceQuestion
+	 */
+	public function addAnswerOption(Option $option) {
+
+	}
+
+	/**
+	 * 
+	 * @param Option $option
+	 * @return ChoiceQuestion
+	 */
+	public function removeAnswerOption(Option $option) {
+
+	}
+
+	/**
+	 * 
+	 * @return array[Option]
+	 */
+	public function getAnswer() {
+
+	}
+
+	/**
+	 * 
+	 * @param mixed $id
+	 * @return Option
+	 * @throws ItemDoesNotExistException
+	 */
+	public function getAnswerOption($id) {
+
+	}
+
+	/**
+	 * 
+	 * @param Option $option
+	 * @return boolean
+	 */
+	public function hasAnswerOption(Option $option) {
+
+	}
+
+	/**
+	 * 
+	 * @return Respondent
+	 */
+	public function getRespondent() {
+
+	}
+
+	/**
+	 * 
+	 * @param Respondent $respondent
+	 */
+	public function setRespondent(Respondent $respondent) {
+
+	}
+
+	/**
+	 * 
+	 * @return \Poll\PollBundle\Entity\Question
+	 */
+	public function getQuestion() {
+
+	}
+
+	/**
+	 * 
+	 * @param Question $question
+	 * @return \Poll\PollBundle\Entity\Answer
+	 * @throws \LogicException pokud trida/rozhrani, se kterou je odpoved kompatibilni, neexistuje
+	 * @throws IncompatibleClassException otazka neni kompatibilni s touto odpovedi
+	 */
+	public function setQuestion(Question $question) {
+
+	}
+}
