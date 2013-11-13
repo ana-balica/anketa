fc2a3254db59fa75fdf79b3283713f48253efa60
ana-balica <ana.balica@gmail.com>
2013-11-13 21:49:01 +0100

    add exception ItemDoesNotExistException

---
+++ src/Poll/PollBundle/Exception/ItemDoesNotExistException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Poll\PollBundle\Exception;
+
+/**
+ * Item Does Not Exist Exception class
+ *
+ * @author AnaBalica
+ */
+class ItemDoesNotExistException extends \Exception {
+	
+}
