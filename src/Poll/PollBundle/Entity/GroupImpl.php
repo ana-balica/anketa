b55b88fe4b80bf29e442f8955f7c510a98cb89c1
ana-balica <ana.balica@gmail.com>
2013-11-14 16:14:07 +0100

    scheme of class GroupImpl

---
+++ src/Poll/PollBundle/Entity/GroupImpl.php
@@ -0,0 +1,84 @@
+<?php
+namespace Poll\PollBundle\Entity;
+
+class GroupImpl extends PollItemImpl implements Group {
+
+	/**
+	 * 
+	 * @param Question $question
+	 * @return Group
+	 */
+	public function addQuestion(Question $question) {
+
+	}
+
+	/**
+	 * 
+	 * @param Question $question
+	 * @return Group
+	 */
+	public function removeQuestion(Question $question) {
+
+	}
+
+	/**
+	 * 
+	 * @return array[Question]
+	 */
+	public function getQuestions() {
+
+	}
+
+	/**
+	 * 
+	 * @param mixed $id
+	 * @return Question
+	 * @throws ItemDoesNotExistException
+	 */
+	public function getQuestion($id) {
+
+	}
+
+	/**
+	 * 
+	 * @param Question $question
+	 * @return boolean
+	 */
+	public function hasQuestion(Question $question) {
+
+	}
+
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getTitle() {
+
+	}
+
+	/**
+	 * 
+	 * @param string $title
+	 * @return Group
+	 */
+	public function setTitle($title) {
+
+	}
+
+	/**
+	 * Vrati popis skupiny
+	 * @return string
+	 */
+	public function getDescription() {
+
+	}
+	
+	/**
+	 * 
+	 * @param string $description
+	 * @return Group
+	 */
+	public function setDescription($description) {
+		
+	}
+}
