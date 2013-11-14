bef592536b739d442663a76d81611c8eaefe9de1
ana-balica <ana.balica@gmail.com>
2013-11-14 16:07:52 +0100

    schema of class SingleChoiceAnswerImpl

---
+++ src/Poll/PollBundle/Entity/Choice/SingleChoiceAnswerImpl.php
@@ -0,0 +1,14 @@
+<?php
+namespace Poll\PollBundle\Entity;
+
+class SingleChoiceAnswerImpl extends ChoiceAnswerImpl implements SingleChoiceAnswer {
+
+	/**
+	 * 
+	 * @param Option $answer
+	 * @return SingleChoiceAnswer
+	 */
+	public function setAnswer(Option $answer) {
+
+	}
+}
