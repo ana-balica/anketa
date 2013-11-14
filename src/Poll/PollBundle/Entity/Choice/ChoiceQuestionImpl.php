9b50d9b92d1079771f126251dcfe1927a0f48fdc
ana-balica <ana.balica@gmail.com>
2013-11-14 17:54:28 +0100

    empty implementations in class ChoiceQuestionImpl

--- src/Poll/PollBundle/Entity/Choice/ChoiceQuestionImpl.php
+++ src/Poll/PollBundle/Entity/Choice/ChoiceQuestionImpl.php
@@ -4,5 +4,52 @@ namespace Poll\PollBundle\Entity\Choice;
 use Poll\PollBundle\Entity\QuestionImpl;
 
 abstract class ChoiceQuestionImpl extends QuestionImpl implements ChoiceQuestion {
+
+	/** @var Collection */
+	protected $items;
+
+	/**
+	 * 
+	 * @param \Poll\PollBundle\Entity\Choice\Option $option
+	 * @return \Poll\PollBundle\Entity\Choice\ChoiceQuestion
+	 */
+	public function addOption(Option $option) {
+
+	}
+
+	/**
+	 * 
+	 * @param \Poll\PollBundle\Entity\Choice\Option $option
+	 * @return \Poll\PollBundle\Entity\Choice\ChoiceQuestion
+	 */
+	public function removeOption(Option $option) {
+
+	}
+
+	/**
+	 * 
+	 * @return array[\Poll\PollBundle\Entity\Choice\Option]
+	 */
+	public function getOptions() {
+
+	}
 	
+	/**
+	 * 
+	 * @param mixed $id
+	 * @return \Poll\PollBundle\Entity\Choice\Option
+	 * @throws \Poll\PollBundle\Exception\ItemDoesNotExistException
+	 */
+	public function getOption($id) {
+
+	}
+
+	/**
+	 * 
+	 * @param \Poll\PollBundle\Entity\Choice\Option $option
+	 * @return boolean
+	 */
+	public function hasOption(Option $option) {
+		
+	}
 }
