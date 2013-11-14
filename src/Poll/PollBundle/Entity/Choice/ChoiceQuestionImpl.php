5065450c6b8d40c830afaae7b4561d65448c6432
ana-balica <ana.balica@gmail.com>
2013-11-14 16:06:27 +0100

    definition of abstract class ChoiceQuestionImpl

---
+++ src/Poll/PollBundle/Entity/Choice/ChoiceQuestionImpl.php
@@ -0,0 +1,8 @@
+<?php
+namespace Poll\PollBundle\Entity\Choice;
+
+use Poll\PollBundle\Entity\QuestionImpl;
+
+abstract class ChoiceQuestionImpl extends QuestionImpl implements ChoiceQuestion {
+	
+}
