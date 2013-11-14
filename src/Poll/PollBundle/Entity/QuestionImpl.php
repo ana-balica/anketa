0f7d739ca36900a91d8ae2736c6bb973832b649f
ana-balica <ana.balica@gmail.com>
2013-11-14 16:08:47 +0100

    schema of abstract class QuestionImpl

---
+++ src/Poll/PollBundle/Entity/QuestionImpl.php
@@ -0,0 +1,67 @@
+<?php
+namespace Poll\PollBundle\Entity;
+
+abstract class QuestionImpl extends PollItemImpl implements Question {
+
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getQuestion() {
+
+	}
+
+	/**
+	 * 
+	 * @param string $question
+	 * @return Question
+	 */
+	public function setQuestion($question) {
+
+	}
+
+	/**
+	 * 
+	 * @param Answer $answer
+	 * @return Question
+	 */
+	public function addAnswer(Answer $answer) {
+
+	}
+
+	/**
+	 * 
+	 * @param Answer $answer
+	 * @return Question
+	 */
+	public function removeAnswer(Answer $answer) {
+
+	}
+
+	/**
+	 * 
+	 * @return array[Answer]
+	 */
+	public function getAnswers() {
+
+	}
+
+	/**
+	 * 
+	 * @param string $id
+	 * @return Question
+	 * @throws ItemDoesNotExistException
+	 */
+	public function getAnswer($id) {
+
+	}
+
+	/**
+	 * Overi, ze mezi odpovedmi je i odpoved
+	 * @param Answer $answer
+	 * @return boolean
+	 */
+	public function hasAnswer(Answer $answer) {
+		
+	}
+}
