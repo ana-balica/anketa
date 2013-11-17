65d8875638fa45d5b53970dfc5d1261cbb9c7930
ana-balica <ana.balica@gmail.com>
2013-11-17 13:48:35 +0100

    moved the methods to AnswerImpl class

--- src/Poll/PollBundle/Entity/Choice/MultipleChoiceAnswerImpl.php
+++ src/Poll/PollBundle/Entity/Choice/MultipleChoiceAnswerImpl.php
@@ -69,24 +69,5 @@ class MultipleChoiceAnswerImpl extends ChoiceAnswerImpl implements MultipleChoic
 	 */
 	public function setRespondent(Respondent $respondent) {
 
-	}
-
-	/**
-	 * 
-	 * @return \Poll\PollBundle\Entity\Question
-	 */
-	public function getQuestion() {
-
-	}
-
-	/**
-	 * 
-	 * @param Question $question
-	 * @return \Poll\PollBundle\Entity\Answer
-	 * @throws \LogicException pokud trida/rozhrani, se kterou je odpoved kompatibilni, neexistuje
-	 * @throws IncompatibleClassException otazka neni kompatibilni s touto odpovedi
-	 */
-	public function setQuestion(Question $question) {
-
-	}
+	}	
 }
