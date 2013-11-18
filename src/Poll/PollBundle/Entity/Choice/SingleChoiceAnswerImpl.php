189753f8501efa123d132add96a082418c8aff85
ana-balica <ana.balica@gmail.com>
2013-11-18 12:46:54 +0100

    SingleChoiceAnswerImpl class

--- src/Poll/PollBundle/Entity/Choice/SingleChoiceAnswerImpl.php
+++ src/Poll/PollBundle/Entity/Choice/SingleChoiceAnswerImpl.php
@@ -1,14 +1,20 @@
 <?php
 namespace Poll\PollBundle\Entity\Choice;
 
+/**
+ * Class SingleChoiceAnswerImpl
+ * Represents an answer with a single choice
+ * @package Poll\PollBundle\Entity\Choice
+ */
 class SingleChoiceAnswerImpl extends ChoiceAnswerImpl implements SingleChoiceAnswer {
 
 	/**
-	 * 
+	 * Set the answer
 	 * @param Option $answer
 	 * @return SingleChoiceAnswer
 	 */
 	public function setAnswer(Option $answer) {
-
+		$this->answer = $answer;
+		return $this;
 	}
 }
