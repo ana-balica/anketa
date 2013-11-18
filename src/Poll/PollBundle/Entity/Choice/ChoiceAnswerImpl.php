60ad0d70573da42dd30ff2a31e014ea59fa86c93
ana-balica <ana.balica@gmail.com>
2013-11-18 12:29:47 +0100

    abstract class ChoiceAnswerImpl

--- src/Poll/PollBundle/Entity/Choice/ChoiceAnswerImpl.php
+++ src/Poll/PollBundle/Entity/Choice/ChoiceAnswerImpl.php
@@ -3,12 +3,20 @@ namespace Poll\PollBundle\Entity\Choice;
 
 use Poll\PollBundle\Entity\AnswerImpl;
 
+/**
+ * Abstract class for general purpose ChoiceAnswer classes
+ * @author AnaBalica
+ */
 abstract class ChoiceAnswerImpl extends AnswerImpl implements ChoiceAnswer {
 
+	/** @var Option */
+	protected $answer;
+	
 	/**
+	 * Get the answer
 	 * @return Option|Options
 	 */
 	public function getAnswer() {
-		
+		return $this->answer;
 	}
 }
