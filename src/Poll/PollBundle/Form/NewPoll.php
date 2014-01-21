8b43cd068a856842bb4dffb5dfc814ce9b61c006
ana-balica <ana.balica@gmail.com>
2014-01-21 12:36:53 +0100

    create a form for new polls

---
+++ src/Poll/PollBundle/Form/NewPoll.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Poll\PollBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class NewPoll extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('Submit', 'submit');
+    }
+
+    public function getName() {
+        return 'newpoll';
+    }
+} 
