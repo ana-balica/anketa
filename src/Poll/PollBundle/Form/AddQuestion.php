338ef809d18bb10d176a3bcb12e3ba10c698b760
ana-balica <ana.balica@gmail.com>
2014-01-21 17:48:30 +0100

    add question form and render the form in a template

---
+++ src/Poll/PollBundle/Form/AddQuestion.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ana
+ * Date: 1/21/14
+ * Time: 12:42 PM
+ */
+
+namespace Poll\PollBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class AddQuestion extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('type', 'choice', array(
+                'choices' => array('text' => 'Text question',
+                                    'single' => 'Single choice question',
+                                    'multiple' => 'Multiple choice question')))
+            ->add('question_text', 'text')
+            ->add('options', 'textarea', array('required' => False))
+            ->add('addquestion', 'submit', array('label' => 'Add question'));
+    }
+
+    public function getName() {
+        return 'addquestion';
+    }
+
+} 
