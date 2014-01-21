32f9fee1498fe6a03a6c6c56dc8f287622c621c9
ana-balica <ana.balica@gmail.com>
2014-01-21 18:09:40 +0100

    basic add question action

--- src/Poll/PollBundle/Controller/DefaultController.php
+++ src/Poll/PollBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Poll\PollBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Poll\PollBundle\Form\NewPoll;
+use Poll\PollBundle\Form\AddQuestion;
 
 class DefaultController extends Controller
 {
@@ -32,7 +33,7 @@ class DefaultController extends Controller
 //    }
 
     /**
-     * Create a poll by providing it's title and short description
+     * Display and submit a form with a poll by providing it's title and short description
      *
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
@@ -51,6 +52,29 @@ class DefaultController extends Controller
         return $this->render('PollPollBundle:Poll:create_poll.html.twig', array('form' => $form->createView()));
     }
 
+    /**
+     * Add a question of any of 3 types to the poll
+     *
+     * @param Request $request
+     * @param $poll_id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function addquestionAction(Request $request, $poll_id)
+    {
+//        \Doctrine\Common\Util\Debug::dump($poll_id);
+//        Extract the title of the poll
+        $title = "Some dummy title";
+        $form = $this->createForm(new AddQuestion());
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $next_action = $form->get('done')->isClicked() ? 'poll_show' : 'poll_add_question';
+            return $this->redirect($this->generateUrl($next_action, array("poll_id" => $poll_id)));
+        }
+        return $this->render('PollPollBundle:Poll:add_question.html.twig', array(
+            'form' => $form->createView(),
+            'title' => $title));
+    }
 
     public function showpollAction(Request $request, $poll_id) {
         return $this->render('PollPollBundle:Default:index.html.twig');
