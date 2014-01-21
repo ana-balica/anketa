4b2d1f6ab719f3a9ea951fc12e10c54bf5748383
ana-balica <ana.balica@gmail.com>
2014-01-21 12:40:55 +0100

    description field from newpoll form is optional

--- src/Poll/PollBundle/Form/NewPoll.php
+++ src/Poll/PollBundle/Form/NewPoll.php
@@ -10,7 +10,7 @@ class NewPoll extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('title', 'text')
-            ->add('description', 'textarea')
+            ->add('description', 'textarea', array('required' => False))
             ->add('Submit', 'submit');
     }
 
