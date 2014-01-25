56a765421c57d7b430eea81df632f9bf4ee6ebbb
ana-balica <ana.balica@gmail.com>
2014-01-25 15:30:47 +0100

    define vars in js explicitly

--- src/Poll/PollBundle/Resources/public/js/script.js
+++ src/Poll/PollBundle/Resources/public/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	// add question template - show/hide the options textarea depending on the selected option
 	$("select").change(function() {
 	    $( "select option:selected" ).each(function() {
-			$value = $(this).val();
+			var $value = $(this).val();
 		    if ($value == 1) {
 			    $("textarea").hide();
 		    } else if ($value == 2 || $value == 3) {
