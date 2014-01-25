860c791141a56c2c4fda2d332cd5702ac8846e2f
ana-balica <ana.balica@gmail.com>
2014-01-25 15:31:10 +0100

    edit\delete labels for questions

--- src/Poll/PollBundle/Resources/public/js/script.js
+++ src/Poll/PollBundle/Resources/public/js/script.js
@@ -13,6 +13,20 @@ $(document).ready(function () {
 	    });
 	}).trigger( "change" );
 
+	// inject the question id to the Edit and Delete labels
+	$(".form-group div").each(function() {
+		$this = $(this);
+		var $form_id = $this.attr('id');
+		if ($form_id !== undefined) {
+			var $id = $form_id.replace(/^.+_/, '');
+			$this.siblings(".action-label").each(function() {
+				var $href = $(this).attr('href');
+				var $new_href = $href.slice(0, -1) + $id;
+				$(this).attr('href', $new_href);
+			});
+		}
+	});
+
 });
 
 
